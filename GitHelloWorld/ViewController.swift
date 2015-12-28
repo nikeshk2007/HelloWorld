675e56441da568b20039332e2b23b08378331ca9
nikesh <nikeshk2007@gmail.com>
2015-12-28 15:45:40 +0530

    Initial Commit

---
+++ GitHelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHelloWorld
+//
+//  Created by Nikesh on 28/12/15.
+//  Copyright © 2015 IMS Health. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
